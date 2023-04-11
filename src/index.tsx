e68ac276fe1e9b2eef19cb4bacc72016bd530b87
Juan Piza Pinzon <jpizapinzon@splunk.com>
2023-04-11 12:55:43 -0500

    Some changes for redirect and routing

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <PokemonProvider>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>
 </PokemonProvider>
